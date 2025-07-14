3f62f14fc9fe49527dde0fb544d93f4f36afdca2
HASUMI Hitoshi <hasumikin@gmail.com>
2025-07-14 18:05:28 +0900

    ref https://github.com/mrubyc/mrubyc/pull/240

--- compiler_test/gen_values_test.rb
+++ compiler_test/gen_values_test.rb
@@ -3,7 +3,7 @@ class GenValuesTest < PicoRubyTest
   if @@vm_select == :mruby
 
     desc "gen_values() in generator.c"
-    assert_equal(<<~RUBY, (@@vm_select == :mruby ? "1\n[2, 3, 4]\n{a: 0}\n{}\n:hello" : "1\n[2, 3, 4]\n{:a=>0}\n{}\n:hello"))
+    assert_equal(<<~RUBY, "1\n[2, 3, 4]\n{a: 0}\n{}\n:hello")
       a = ->(m,*rest,m2,**opts,&block) do
         p m,rest,m2,opts;
         block.call
