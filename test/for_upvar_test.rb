40e17ecf6737d39f6560895594c9ba8cc9abbae9
HASUMI Hitoshi <hasumikin@gmail.com>
2026-02-18 13:44:40 +0900

    Inherit for_depth if block exists

--- test/for_upvar_test.rb
+++ test/for_upvar_test.rb
@@ -40,4 +40,37 @@ class ForLoopUpvarTest < Picotest::Test
     actual = run_script(script)
     assert_equal("[]\n[]\n[]", actual)
   end
+
+  def test_block_inside_for_accesses_for_variable
+    script = <<~RUBY
+      for x in [1, 2, 3]
+        [0].each { p x }
+      end
+    RUBY
+    actual = run_script(script)
+    assert_equal("1\n2\n3", actual)
+  end
+
+  def test_block_inside_nested_for_accesses_for_variables
+    script = <<~RUBY
+      for m in [1, 2]
+        for n in [10, 20]
+          [0].each { p [m, n] }
+        end
+      end
+    RUBY
+    actual = run_script(script)
+    assert_equal("[1, 10]\n[1, 20]\n[2, 10]\n[2, 20]", actual)
+  end
+
+  def test_block_inside_for_accesses_outer_and_for_variables
+    script = <<~RUBY
+      base = 100
+      for x in [1, 2]
+        [0].each { p base + x }
+      end
+    RUBY
+    actual = run_script(script)
+    assert_equal("101\n102", actual)
+  end
 end
