e15c7f49b631f9463ddb77a4d6da930eddad33a3
HASUMI Hitoshi <hasumikin@gmail.com>
2026-02-19 19:04:29 +0900

    Fix PM_BEGIN_NODE
    - If rescue_clause exits and val==1, insert `pop()` before calling gen_ensure

--- test/ensure_test.rb
+++ test/ensure_test.rb
@@ -13,4 +13,21 @@ class EnsureTest < Picotest::Test
     actual = run_script(script)
     assert_equal("2\n1", actual)
   end
+
+  def test_begin_rescue_else_ensure_return_value
+    script = <<~RUBY
+      r = begin
+        1+1
+      rescue
+        2+2
+      else
+        3+3
+      ensure
+        4+4
+      end
+      p r
+    RUBY
+    actual = run_script(script)
+    assert_equal("6", actual)
+  end
 end
