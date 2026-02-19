b96467efe9b006d052895ac8a9017c7aa09bacd9
HASUMI Hitoshi <hasumikin@gmail.com>
2026-02-19 10:49:29 +0900

    git rm -r obsolete_compiler_test

--- obsolete_compiler_test/lvar_test.rb
+++
@@ -1,6 +0,0 @@
-class LvarTest < PicoRubyTest
-  desc "Implicit initialize"
-  assert_equal(<<~RUBY, 'nil')
-    p(a=a)
-  RUBY
-end
--- obsolete_compiler_test/rightward_assign_test.rb
+++
@@ -1,6 +0,0 @@
-class RightwardAssignTest < PicoRubyTest
-  assert_equal(<<~RUBY, "hello")
-    :hello.to_s => a
-    puts a
-  RUBY
-end
